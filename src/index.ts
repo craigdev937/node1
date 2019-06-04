4feb89b8439602fbb8d9185c2b8ad3ebdbff40d8
indigomx9 <indigomx9@football-mx9.fios-router.home>
2019-06-04 14:13:15 -0400

    Initial setup of a Nodejs and TypeScript exercise.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Person } from "./person";
+
+const person1 = new Person("Henry Quartermain", 18);
+
+person1.greeting();
+
+
+
---
+++ src/person.ts
@@ -0,0 +1,18 @@
+
+
+export class Person {
+    name: string
+    age: number
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    };
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`);
+    };
+};
+
+
+
